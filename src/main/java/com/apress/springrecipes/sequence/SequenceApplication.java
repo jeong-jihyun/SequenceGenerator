68ac1f3b3b0ebf8970161de30fc52aeac11e961d
정지현 <mujuk73@hotmail.com>
2025-01-11 17:40:46 +0900

    Ioc 컨테이너에서 POJO 인스턴스/빈 가져오기

--- src/main/java/com/apress/springrecipes/sequence/SequenceApplication.java
+++ src/main/java/com/apress/springrecipes/sequence/SequenceApplication.java
@@ -3,6 +3,7 @@ package com.apress.springrecipes.sequence;
 import com.apress.springrecipes.sequence.Entity.SequenceGenerator;
 import com.apress.springrecipes.sequence.config.SequenceGeneratorConfiguration;
 
+import com.apress.springrecipes.sequence.dao.SequenceDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -31,13 +32,20 @@ public class SequenceApplication {
     public static void main(String[] args) {
         // Ioc 컨테이너를 초기화하여 애너테이션 스케닝하기
         ApplicationContext context = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
-        log.info("Sequence: " + context.getBean(SequenceGenerator.class).getSequence());
+        log.info("Sequence: {}", context.getBean(SequenceGenerator.class).getSequence());
         // Ioc 컨테이너에서 POJO 인스턴스/빈을 가져오기
         SequenceGenerator generator = context.getBean(SequenceGenerator.class);
         SequenceGenerator generator2 = context.getBean("sequenceGenerator", SequenceGenerator.class);
 
-        log.info("Sequence: " + generator.getSequence());
-        log.info("Sequence: " + generator2.getSequence());
+        log.info("Sequence: {}", generator.getSequence());
+        log.info("Sequence: {}", generator2.getSequence());
+
+
+        ApplicationContext context1 = new AnnotationConfigApplicationContext("com.apress.springrecipes.sequence");
+        SequenceDao sequenceDao = context1.getBean(SequenceDao.class);
+
+        log.info("Sequence: {}", sequenceDao.getNextValue("IT"));
+        log.info("Sequence: {}", sequenceDao.getNextValue("IT"));
 
         SpringApplication.run(SequenceApplication.class, args);
     }
