a2f6391c9dbcaa75f68b038436a5c6d65eff71ce
정지현 <mujuk73@hotmail.com>
2025-01-11 16:31:54 +0900

    first commit

---
+++ src/main/java/com/apress/springrecipes/sequence/SequenceApplication.java
@@ -0,0 +1,13 @@
+package com.apress.springrecipes.sequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SequenceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/apress/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
@@ -0,0 +1,17 @@
+package com.apress.springrecipes.sequence.config;
+
+import com.apress.springrecipes.sequence.SequenceGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SequenceGeneratorConfiguration {
+    @Bean
+    public SequenceGenerator sequenceGenerator() {
+        SequenceGenerator sequenceGenerator = new SequenceGenerator();
+        sequenceGenerator.setPrefix("30");
+        sequenceGenerator.setSuffix("A");
+        sequenceGenerator.setInitial(100000);
+        return sequenceGenerator;
+    }
+}
